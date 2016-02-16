d567c4624278826a3b74f2fa139e0ebe1370527e
huro3h_satoshi <satoshiii@satoshiii-no-MacBook-Pro.local>
2016-02-16 15:50:20 +0800

    いらないところ

--- views/blogs/index.php
+++ views/blogs/index.php
@@ -7,27 +7,3 @@
             [<a href="" style="color: #F33;">削除</a>]
           </p>
         </div>
-        <div class="msg">
-          <p><a href="show.html">タイトル３</a></p>
-          <p class="day">
-              2016-01-28 18:04
-            [<a href="edit.html" style="color: #00994C;">編集</a>]
-            [<a href="" style="color: #F33;">削除</a>]
-          </p>
-        </div>
-        <div class="msg">
-          <p><a href="show.html">タイトル２</a></p>
-          <p class="day">
-              2016-01-28 18:04
-            [<a href="edit.html" style="color: #00994C;">編集</a>]
-            [<a href="" style="color: #F33;">削除</a>]
-          </p>
-        </div>
-        <div class="msg">
-          <p><a href="show.html">タイトル１</a></p>
-          <p class="day">
-              2016-01-28 18:04
-            [<a href="edit.html" style="color: #00994C;">編集</a>]
-            [<a href="" style="color: #F33;">削除</a>]
-          </p>
-        </div>
