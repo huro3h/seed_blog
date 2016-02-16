b289f5d176fc974c0067bfbcb57525f46ac8717f
huro3h_satoshi <satoshiii@satoshiii-no-MacBook-Pro.local>
2016-02-16 15:44:08 +0800

    てst

--- models/blog.php
+++ models/blog.php
@@ -1,8 +1,34 @@
 <?php 
 
 	class Blog {
+
+
+		//プロパティに設定
+		private $dbconnect = '';
+
+		//NEWされる時最初にいるのは？ →コンストラクタ
+		public function __construct(){
+			// DB接続ファイルの読み込み
+			require('dbconnect.php');
+			// DB接続設定の値を代入
+			$this->dbconnect = $db;
+		}
+
 		public function index() {
-			echo 'モデルのindex( )メソッドが呼ばれました☆';
+			// DB接続用意 ↓ MYSQLから引っ張ってくる
+			// $sql = 'SELECT * FROM `blogs` WHERE `delete_flag` = 0';
+			$sql = 'SELECT * FROM `blogs` WHERE `delete_flag` = 0';
+			$results = mysqli_query($this->dbconnect, $sql) or die(mysqli_error($this->dbconnect));
+
+
+			// 連想配列で順番にでてきたのを格納する変数
+			
+			while ($result = mysqli_fetch_assoc($results)) {
+				//今回表示したいのが、タイトルと日付とid
+				echo $result['id'];
+				echo $result['title'];
+				echo $result['created'];
+			}
 		}
 	}
 
