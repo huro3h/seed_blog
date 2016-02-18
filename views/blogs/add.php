0bf3515158e2f8913a964b58bd6185f88e2efc0b
huro3h_satoshi <iges11@gmail.com>
2016-02-18 14:21:43 +0800

    add.php 1,ボタンをsubmitに変更 と formのアクションを指定

--- views/blogs/add.php
+++ views/blogs/add.php
@@ -1,5 +1,5 @@
         <div class="msg">
-          <form method="post" action="" class="form-horizontal" role="form">
+          <form method="post" action="/see_blog/blogs/create" class="form-horizontal" role="form">
             <div class="form-group">
               <label for="name" class="col-md-3 control-label">タイトル</label>
               <div class="col-md-9">
@@ -15,7 +15,8 @@
             <div class="form-group pull-right">
               <p>
                 <a href="/seed_blog/blogs/index" class="btn btn-default">戻る</a>&nbsp;&nbsp;
-                <a href="index.html" class="btn btn-success">投稿する</a>
+                <!-- <a href="index.html" class="btn btn-success">投稿する</a> -->
+                <input type="submit" class="btn btn-success" value="投稿する！">
               </p>
             </div>
           </form>
